fed9fb768866823fd4a7acf86413ed071956f8e6
zhaoweihao <1028410337@qq.com>
2018-02-20 16:35:07 +0800

    finish post request

--- app/src/main/java/me/zhaoweihao/shopping/ContactFragment.kt
+++ app/src/main/java/me/zhaoweihao/shopping/ContactFragment.kt
@@ -7,7 +7,6 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import me.zhaoweihao.shopping.DynamicFragment
 import me.zhaoweihao.shopping.R
 
 
