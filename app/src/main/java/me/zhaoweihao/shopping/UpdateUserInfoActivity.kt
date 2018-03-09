f0ce4cf21447fa16d02c3dfd28e4df86386c5fa0
zhaoweihaoChina <1028410337@qq.com>
2018-03-09 14:38:40 +0800

    完成个人信息页面点击按钮生成json数据，准备上传到服务器

--- app/src/main/java/me/zhaoweihao/shopping/UpdateUserInfoActivity.kt
+++ app/src/main/java/me/zhaoweihao/shopping/UpdateUserInfoActivity.kt
@@ -3,10 +3,12 @@ package me.zhaoweihao.shopping
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.gson.Gson
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_update_user_info.*
 import kotlinx.android.synthetic.main.fragment_user.*
 import me.zhaoweihao.shopping.constant.Constant
+import me.zhaoweihao.shopping.gson.Update
 import me.zhaoweihao.shopping.litepal.UserInfo
 import org.litepal.crud.DataSupport
 
@@ -54,6 +56,42 @@ class UpdateUserInfoActivity : AppCompatActivity() {
             when(userAuthenticated) {
                 0 -> tv_authenticated.text = "实名状态：未实名"
                 1 -> tv_authenticated.text = "实名状态：已实名"
+            }
+
+            /**
+             *
+             * @param {*} token
+             * @param {*} id
+             * @param {*} user_name
+             * @param {*} user_sex
+             * @param {*} user_phone
+             * @param {*} user_avator 头像
+             * @param { Array } user_address 所在区域 数组的方式 ["广东","惠州","博罗"]
+             * @param { string } user_address_more 地址补充
+             */
+
+            btn_sure.setOnClickListener {
+                val update = Update()
+                update.token = find.userToken
+                update.id = find.userId
+                update.userName = et_username.text.toString()
+
+                if(rb_male.isChecked) {
+                    update.userSex = 0
+                } else {
+                    update.userSex = 1
+                }
+
+                update.userPhone = et_userphone.text.toString()
+                update.userAvator = find.userAvator
+                update.userAddress = et_useraddress.text.toString()
+                update.userAddressMore = et_useraddress_more.text.toString()
+
+                val jsonObject = Gson().toJson(update)
+                Log.d(TAG,jsonObject)
+
+                //将数据通过POST方法上传到服务器
+                
             }
 
 
